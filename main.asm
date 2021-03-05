680e729455b8a49bae3b16726e729528c32c6be7
DesaulniersM <Mdesaulniersm@gmail.com>
2021-03-04 23:18:53 -0700

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,28 @@
+;
+; FirstAssembly.asm
+;
+; Created: 1/25/2019 1:09:52 PM
+; Author : Matthew
+;
+; This code will find the first 100 numbers in the fibonacci sequence. After that it will restart from the beginning
+
+; Replace with your application code
+start:
+
+;initialize loop restart register
+	
+	ldi r16, 1
+	ldi r19, 10
+;Begin Fibonacci loop
+
+nacci:
+	inc r20
+	mov r17, r16
+	add r16, r18
+
+	mov r18, r17
+	cp r20, r19
+	brge 27
+	jmp nacci
+
+    rjmp start
